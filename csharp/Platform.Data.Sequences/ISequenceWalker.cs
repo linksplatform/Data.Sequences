2db5e3b5768218a61a97a160328e612dd34b8541
FreePhoenix <freephoenix888@gmail.com>
2021-08-14 15:49:56 +0600

    Add library code

---
+++ csharp/Platform.Data.Sequences/ISequenceWalker.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+
+namespace Platform.Data.Sequences
+{
+    public interface ISequenceWalker<TLinkAddress>
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        IEnumerable<IList<TLinkAddress>> Walk(TLinkAddress sequence);
+    }
+}
---
+++ csharp/Platform.Data.Sequences/StopableSequenceWalker.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+
+namespace Platform.Data.Sequences
+{
+    /// <remarks>
+    /// Реализованный внутри алгоритм наглядно показывает,
+    /// что совершенно не обязательна рекурсивная реализация (с вложенным вызовом функцией самой себя),
+    /// так как стэк можно использовать намного эффективнее при ручном управлении.
+    /// 
+    /// Решить объединять ли логику в одну функцию, или оставить 4 отдельных реализации?
+    /// Решить встраивать ли защиту от зацикливания.
+    /// Альтернативой защиты от закливания может быть заранее известное ограничение на погружение вглубь.
+    /// А так же качественное распознавание прохода по циклическому графу.
+    /// Ограничение на уровень глубины рекурсии может позволить использовать уменьшенный размер стека.
+    /// Можно использовать глобальный стек (или несколько глобальных стеков на каждый поток).
+    /// </remarks>
+    public static class StopableSequenceWalker
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool WalkRight<TLinkAddress>(TLinkAddress sequence, Func<TLinkAddress, TLinkAddress> getSource, Func<TLinkAddress, TLinkAddress> getTarget, Func<TLinkAddress, bool> isElement, Action<TLinkAddress> enter, Action<TLinkAddress> exit, Func<TLinkAddress, bool> canEnter, Func<TLinkAddress, bool> visit)
+        {
+            var exited = 0;
+            var stack = new Stack<TLinkAddress>();
+            var element = sequence;
+            if (isElement(element))
+            {
+                return visit(element);
+            }
+            while (true)
+            {
+                if (isElement(element))
+                {
+                    if (stack.Count == 0)
+                    {
+                        return true;
+                    }
+                    element = stack.Pop();
+                    exit(element);
+                    exited++;
+                    var source = getSource(element);
+                    var target = getTarget(element);
+                    if ((isElement(source) || (exited == 1 && !canEnter(source))) && !visit(source))
+                    {
+                        return false;
+                    }
+                    if ((isElement(target) || !canEnter(target)) && !visit(target))
+                    {
+                        return false;
+                    }
+                    element = target;
+                }
+                else
+                {
+                    if (canEnter(element))
+                    {
+                        enter(element);
+                        exited = 0;
+                        stack.Push(element);
+                        element = getSource(element);
+                    }
+                    else
+                    {
+                        if (stack.Count == 0)
+                        {
+                            return true;
+                        }
+                        element = stack.Pop();
+                        exit(element);
+                        exited++;
+                        var source = getSource(element);
+                        var target = getTarget(element);
+                        if ((isElement(source) || (exited == 1 && !canEnter(source))) && !visit(source))
+                        {
+                            return false;
+                        }
+                        if ((isElement(target) || !canEnter(target)) && !visit(target))
+                        {
+                            return false;
+                        }
+                        element = target;
+                    }
+                }
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool WalkRight<TLinkAddress>(TLinkAddress sequence, Func<TLinkAddress, TLinkAddress> getSource, Func<TLinkAddress, TLinkAddress> getTarget, Func<TLinkAddress, bool> isElement, Func<TLinkAddress, bool> visit)
+        {
+            var stack = new Stack<TLinkAddress>();
+            var element = sequence;
+            if (isElement(element))
+            {
+                return visit(element);
+            }
+            while (true)
+            {
+                if (isElement(element))
+                {
+                    if (stack.Count == 0)
+                    {
+                        return true;
+                    }
+                    element = stack.Pop();
+                    var source = getSource(element);
+                    var target = getTarget(element);
+                    if (isElement(source) && !visit(source))
+                    {
+                        return false;
+                    }
+                    if (isElement(target) && !visit(target))
+                    {
+                        return false;
+                    }
+                    element = target;
+                }
+                else
+                {
+                    stack.Push(element);
+                    element = getSource(element);
+                }
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool WalkLeft<TLinkAddress>(TLinkAddress sequence, Func<TLinkAddress, TLinkAddress> getSource, Func<TLinkAddress, TLinkAddress> getTarget, Func<TLinkAddress, bool> isElement, Func<TLinkAddress, bool> visit)
+        {
+            var stack = new Stack<TLinkAddress>();
+            var element = sequence;
+            if (isElement(element))
+            {
+                return visit(element);
+            }
+            while (true)
+            {
+                if (isElement(element))
+                {
+                    if (stack.Count == 0)
+                    {
+                        return true;
+                    }
+                    element = stack.Pop();
+                    var source = getSource(element);
+                    var target = getTarget(element);
+                    if (isElement(target) && !visit(target))
+                    {
+                        return false;
+                    }
+                    if (isElement(source) && !visit(source))
+                    {
+                        return false;
+                    }
+                    element = source;
+                }
+                else
+                {
+                    stack.Push(element);
+                    element = getTarget(element);
+                }
+            }
+        }
+    }
+}
