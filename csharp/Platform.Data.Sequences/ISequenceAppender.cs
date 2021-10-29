65da88ae72c4e3ac39a0ba4838158dbc64c25d63
FreePhoenix <freephoenix888@gmail.com>
2021-10-29 21:44:01 +0600

    Revert "Automatically generated XML-comment stubs are added."
    This reverts commit 1dc1ec845a625be294c837c3cefaf73dde6f3f63.

--- csharp/Platform.Data.Sequences/ISequenceAppender.cs
+++ csharp/Platform.Data.Sequences/ISequenceAppender.cs
@@ -1,35 +1,11 @@
-using System.Runtime.CompilerServices;
+﻿using System.Runtime.CompilerServices;
 
 #pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
 
 namespace Platform.Data.Sequences
 {
-    /// <summary>
-    /// <para>
-    /// Defines the sequence appender.
-    /// </para>
-    /// <para></para>
-    /// </summary>
     public interface ISequenceAppender<TLinkAddress>
     {
-        /// <summary>
-        /// <para>
-        /// Appends the sequence.
-        /// </para>
-        /// <para></para>
-        /// </summary>
-        /// <param name="sequence">
-        /// <para>The sequence.</para>
-        /// <para></para>
-        /// </param>
-        /// <param name="appendant">
-        /// <para>The appendant.</para>
-        /// <para></para>
-        /// </param>
-        /// <returns>
-        /// <para>The link address</para>
-        /// <para></para>
-        /// </returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         TLinkAddress Append(TLinkAddress sequence, TLinkAddress appendant);
     }
--- csharp/Platform.Data.Sequences/ISequenceWalker.cs
+++ csharp/Platform.Data.Sequences/ISequenceWalker.cs
@@ -1,32 +1,12 @@
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 
 #pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
 
 namespace Platform.Data.Sequences
 {
-    /// <summary>
-    /// <para>
-    /// Defines the sequence walker.
-    /// </para>
-    /// <para></para>
-    /// </summary>
     public interface ISequenceWalker<TLinkAddress>
     {
-        /// <summary>
-        /// <para>
-        /// Walks the sequence.
-        /// </para>
-        /// <para></para>
-        /// </summary>
-        /// <param name="sequence">
-        /// <para>The sequence.</para>
-        /// <para></para>
-        /// </param>
-        /// <returns>
-        /// <para>An enumerable of i list t link address</para>
-        /// <para></para>
-        /// </returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         IEnumerable<IList<TLinkAddress>> Walk(TLinkAddress sequence);
     }
